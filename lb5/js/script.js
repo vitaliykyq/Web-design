709b3cbe51789b13d1a6f6c617689191c0ed4731
vitaliykyq <v.kyb@ukr.net>
2020-10-31 23:30:19 +0200

    новый файл:    lb2/home_page.html 	новый файл:    lb2/task_1.html 	новый файл:    lb2/task_2.html 	новый файл:    lb3/img/image.png 	новый файл:    lb3/img/image2.png 	новый файл:    lb3/img/image3.png 	новый файл:    lb3/img/image4.png 	новый файл:    lb3/img/image5.png 	новый файл:    lb3/img/image6.png 	новый файл:    lb3/img/img2.jpg 	новый файл:    lb3/img/marker.png 	новый файл:    lb3/index_3.html 	новый файл:    lb3/lists.html 	новый файл:    lb3/schedule.html 	новый файл:    lb3/style/index_style.css 	новый файл:    lb3/style/schedule_style.css 	новый файл:    lb3/style/tables_style.css 	новый файл:    lb3/style/tables_style2.css 	новый файл:    lb3/tables.html 	новый файл:    lb3/tables2.html 	новый файл:    lb4/Text.txt 	новый файл:    lb4/image.png 	новый файл:    lb4/images.png 	новый файл:    lb4/images2.png 	новый файл:    lb4/index1.html 	новый файл:    lb4/index2.html 	новый файл:    lb5/forms.html 	новый файл:    lb5/image/circle.png 	новый файл:    lb5/image/fon1.png 	новый файл:    lb5/image/fon4_top.png 	новый файл:    "lb5/image/fon5_background (\320\272\320\276\320\277\320\270\321\217).png" 	новый файл:    lb5/image/fon5_background.png 	новый файл:    lb5/js/script.js 	новый файл:    lb5/questionnaire.html 	новый файл:    lb5/style.css

---
+++ lb5/js/script.js
@@ -0,0 +1,9 @@
+function myFunctionOne(x) {
+  if(x.value) form_5_Email.setAttribute("style", "display:block");
+  else form_5_Email.setAttribute("style", "display:none");
+}
+
+function myFunctionTwo(x) {
+  if(x.value) form_5_Password.setAttribute("style", "display:block");
+  else form_5_Password.setAttribute("style", "display:none");
+}
